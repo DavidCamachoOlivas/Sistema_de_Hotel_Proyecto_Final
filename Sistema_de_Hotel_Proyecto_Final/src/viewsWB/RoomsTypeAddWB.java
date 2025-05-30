210530707f4674da05f103d89ad91dea62985013
LuisDaniel1505 <lontiveros_23@alu.uabcs.mx>
2025-05-29 18:20:54 -0700

    QA: Nombre variables vista “RoomsTypeAddWB”.

--- Sistema_de_Hotel_Proyecto_Final/src/viewsWB/RoomsTypeAddWB.java
+++ Sistema_de_Hotel_Proyecto_Final/src/viewsWB/RoomsTypeAddWB.java
@@ -3,142 +3,135 @@ package viewsWB;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Rectangle;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
-import javax.swing.JEditorPane;
-import javax.swing.JProgressBar;
-import javax.swing.JToggleButton;
-import javax.swing.JComboBox;
+import javax.swing.border.EmptyBorder;
 
 public class RoomsTypeAddWB extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+    private static final long serialVersionUID = 1L;
+    private JPanel mainPanel;
+    private JTextField roomTypeField;
+    private JTextField rateTypeField;
+    private JTextField selectedRoomsField;
+    private JComboBox<String> floorComboBox;
+    private JComboBox<String> capacityComboBox;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					RoomsTypeAddWB frame = new RoomsTypeAddWB();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                RoomsTypeAddWB frame = new RoomsTypeAddWB();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public RoomsTypeAddWB() {
-		setResizable(false);
-		setBounds(new Rectangle(0, 0, 720, 1280));
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1280, 720);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(255, 252, 247));
-		contentPane.setBounds(new Rectangle(0, 0, 720, 1280));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    /**
+     * Create the frame.
+     */
+    public RoomsTypeAddWB() {
+        setResizable(false);
+        setBounds(new Rectangle(0, 0, 720, 1280));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 1280, 720);
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(7, 26, 43));
-		panel.setBounds(-26, 0, 1290, 119);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		
-		JLabel lblNewLabel_4 = new JLabel("Tipo de habitaciones");
-		lblNewLabel_4.setForeground(Color.WHITE);
-		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 40));
-		lblNewLabel_4.setBounds(238, 21, 490, 66);
-		panel.add(lblNewLabel_4);
-		
-		JLabel lblNewLabel_5 = new JLabel("");
-		lblNewLabel_5.setBounds(146, 21, 128, 75);
-		Image img = new ImageIcon("src/flecha.png").getImage().getScaledInstance(76, 58, Image.SCALE_SMOOTH);
-		lblNewLabel_5.setIcon(new ImageIcon(img));
-		panel.add(lblNewLabel_5);
-		
-		textField = new JTextField();
-		textField.setBounds(122, 196, 470, 49);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(646, 196, 470, 49);
-		contentPane.add(textField_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(122, 327, 994, 49);
-		contentPane.add(textField_2);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(122, 466, 470, 49);
-		contentPane.add(comboBox);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(646, 466, 470, 49);
-		contentPane.add(comboBox_1);
-		
-		JLabel lblNewLabel = new JLabel("Tipo de habitación");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel.setBounds(122, 158, 309, 27);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblTipoDeTarifa = new JLabel("Tipo de tarifa");
-		lblTipoDeTarifa.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblTipoDeTarifa.setBounds(646, 158, 309, 27);
-		contentPane.add(lblTipoDeTarifa);
-		
-		JLabel lblHabitacionesSeleccionadas = new JLabel("Habitaciones seleccionadas");
-		lblHabitacionesSeleccionadas.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblHabitacionesSeleccionadas.setBounds(122, 289, 309, 27);
-		contentPane.add(lblHabitacionesSeleccionadas);
-		
-		JLabel lblPiso = new JLabel("Piso");
-		lblPiso.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblPiso.setBounds(122, 428, 309, 27);
-		contentPane.add(lblPiso);
-		
-		JLabel lblCapacidad = new JLabel("Capacidad");
-		lblCapacidad.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblCapacidad.setBounds(646, 428, 309, 27);
-		contentPane.add(lblCapacidad);
-		
-		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnCancelar.setForeground(Color.WHITE);
-		btnCancelar.setBackground(new Color(153, 89, 45));
-		btnCancelar.setBounds(808, 571, 140, 72);
-		contentPane.add(btnCancelar);
-		
-		JButton btnNewButton_1 = new JButton("Eliminar");
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnNewButton_1.setForeground(Color.WHITE);
-		btnNewButton_1.setBackground(new Color(7, 26, 43));
-		btnNewButton_1.setBounds(976, 571, 140, 72);
-		contentPane.add(btnNewButton_1);
-		
-	}
+        mainPanel = new JPanel();
+        mainPanel.setBackground(new Color(255, 252, 247));
+        mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(mainPanel);
+        mainPanel.setLayout(null);
+
+        JPanel headerPanel = new JPanel();
+        headerPanel.setBackground(new Color(7, 26, 43));
+        headerPanel.setBounds(0, 0, 1290, 119);
+        mainPanel.add(headerPanel);
+        headerPanel.setLayout(null);
+
+        JLabel roomTypeTitleLabel = new JLabel("Tipo de habitaciones");
+        roomTypeTitleLabel.setForeground(Color.WHITE);
+        roomTypeTitleLabel.setFont(new Font("Tahoma", Font.BOLD, 40));
+        roomTypeTitleLabel.setBounds(238, 21, 490, 66);
+        headerPanel.add(roomTypeTitleLabel);
+
+        JLabel backArrowLabel = new JLabel();
+        backArrowLabel.setBounds(146, 21, 128, 75);
+        Image backIcon = new ImageIcon("src/flecha.png").getImage().getScaledInstance(76, 58, Image.SCALE_SMOOTH);
+        backArrowLabel.setIcon(new ImageIcon(backIcon));
+        headerPanel.add(backArrowLabel);
+
+        roomTypeField = new JTextField();
+        roomTypeField.setBounds(122, 196, 470, 49);
+        roomTypeField.setColumns(10);
+        mainPanel.add(roomTypeField);
+
+        rateTypeField = new JTextField();
+        rateTypeField.setBounds(646, 196, 470, 49);
+        rateTypeField.setColumns(10);
+        mainPanel.add(rateTypeField);
+
+        selectedRoomsField = new JTextField();
+        selectedRoomsField.setBounds(122, 327, 994, 49);
+        selectedRoomsField.setColumns(10);
+        mainPanel.add(selectedRoomsField);
+
+        floorComboBox = new JComboBox<>();
+        floorComboBox.setBounds(122, 466, 470, 49);
+        mainPanel.add(floorComboBox);
+
+        capacityComboBox = new JComboBox<>();
+        capacityComboBox.setBounds(646, 466, 470, 49);
+        mainPanel.add(capacityComboBox);
+
+        JLabel roomTypeLabel = new JLabel("Tipo de habitación");
+        roomTypeLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        roomTypeLabel.setBounds(122, 158, 309, 27);
+        mainPanel.add(roomTypeLabel);
+
+        JLabel rateTypeLabel = new JLabel("Tipo de tarifa");
+        rateTypeLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        rateTypeLabel.setBounds(646, 158, 309, 27);
+        mainPanel.add(rateTypeLabel);
+
+        JLabel selectedRoomsLabel = new JLabel("Habitaciones seleccionadas");
+        selectedRoomsLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        selectedRoomsLabel.setBounds(122, 289, 309, 27);
+        mainPanel.add(selectedRoomsLabel);
+
+        JLabel floorLabel = new JLabel("Piso");
+        floorLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        floorLabel.setBounds(122, 428, 309, 27);
+        mainPanel.add(floorLabel);
+
+        JLabel capacityLabel = new JLabel("Capacidad");
+        capacityLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        capacityLabel.setBounds(646, 428, 309, 27);
+        mainPanel.add(capacityLabel);
+
+        JButton cancelButton = new JButton("Cancelar");
+        cancelButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        cancelButton.setForeground(Color.WHITE);
+        cancelButton.setBackground(new Color(153, 89, 45));
+        cancelButton.setBounds(808, 571, 140, 72);
+        mainPanel.add(cancelButton);
+
+        JButton deleteButton = new JButton("Aceptar");
+        deleteButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        deleteButton.setForeground(Color.WHITE);
+        deleteButton.setBackground(new Color(7, 26, 43));
+        deleteButton.setBounds(976, 571, 140, 72);
+        mainPanel.add(deleteButton);
+    }
 }
