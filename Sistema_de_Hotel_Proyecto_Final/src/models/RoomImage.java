590b95b0d0ca143e0adf7ff03d31c8624785afd4
DiegoCareaga946 <dcareaga_23@alu.uabcs.mx>
2025-06-02 09:48:16 -0600

    Back: Clase RoomImage

---
+++ Sistema_de_Hotel_Proyecto_Final/src/models/RoomImage.java
@@ -0,0 +1,42 @@
+package models;
+
+public class RoomImage {
+    private int id_room_image;
+    private int id_room;
+    private byte[] room_image;
+
+
+    public RoomImage(int id_room_image, int id_room, byte[] room_image) {
+        this.id_room_image = id_room_image;
+        this.id_room = id_room;
+        this.room_image = room_image;
+    }
+
+    public RoomImage() {
+    	
+    }
+
+    public int getid_Room_image() {
+        return id_room_image;
+    }
+
+    public void setIdRoomImage(int id_room_image) {
+        this.id_room_image = id_room_image;
+    }
+
+    public int getId_Room() {
+        return id_room;
+    }
+
+    public void setId_Room(int id_room) {
+        this.id_room = id_room;
+    }
+
+    public byte[] getRoom_Image() {
+        return room_image;
+    }
+
+    public void setRoom_Image(byte[] room_image) {
+        this.room_image = room_image;
+    }
+}
