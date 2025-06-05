f0ed2cd9556e4f5737b8a2e33f5edcb7c6138ba6
DavidCamachoOlivas <dac2_23@alu.uabcs.mx>
2025-06-04 23:03:38 -0700

    Front: Creación de view y controller de la clase PopUps

--- Sistema_de_Hotel_Proyecto_Final/src/controllers/PopUpsController.java
+++ Sistema_de_Hotel_Proyecto_Final/src/controllers/PopUpsController.java
@@ -1,5 +1,24 @@
 package controllers;
 
+import views.PopUpsView;
+
 public class PopUpsController {
 
+	public PopUpsView pop = new PopUpsView();
+	
+	public void successDownload() {
+		pop.successDownload();
+	}
+	
+	public void successDelete() {
+		pop.successDelete();
+	}
+	
+	public void successSignIn() {
+		pop.successSignIn();
+	}
+	
+	public void incorrectSignIn() {
+		pop.incorrectSignIn();
+	}
 }
--- Sistema_de_Hotel_Proyecto_Final/src/views/PopUpsView.java
+++ Sistema_de_Hotel_Proyecto_Final/src/views/PopUpsView.java
@@ -1,5 +1,161 @@
 package views;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class PopUpsView {
 
+	JFrame frame = new JFrame();
+	
+	public void successDownload() {
+		frame = new JFrame();
+		frame.setSize(700, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setBackground(Color.decode("#FFFCF7"));//FBF3E6
+		panel.setLayout(null);
+		
+		JLabel title = new JLabel("PDF descargado con exito");
+		title.setBounds(50,100,600,70);
+		title.setFont(new Font("Inter_18pt Bold", Font.PLAIN, 32));
+		title.setVisible(true);
+		panel.add(title);
+		
+		JButton accept = new JButton("Aceptar");
+		accept.setBounds(350,350,300,70);
+		accept.setFont(new Font("Inter_18pt Bold", Font.PLAIN, 32));
+		accept.setForeground(Color.decode("#FFFFFF"));
+		accept.setBackground(Color.decode("#071A2B"));
+		panel.add(accept);
+		
+		accept.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				frame.dispose();
+			}
+			
+		});
+	}
+	
+	public void successDelete() {
+		frame = new JFrame();
+		frame.setSize(700, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setBackground(Color.decode("#FFFCF7"));//FBF3E6
+		panel.setLayout(null);
+		
+		JLabel title = new JLabel("Cliente eliminado con exito");
+		title.setBounds(50,100,600,70);
+		title.setFont(new Font("Inter_18pt Bold", Font.PLAIN, 32));
+		title.setVisible(true);
+		panel.add(title);
+		
+		JButton accept = new JButton("Aceptar");
+		accept.setBounds(350,350,300,70);
+		accept.setFont(new Font("Inter_18pt Bold", Font.PLAIN, 32));
+		accept.setForeground(Color.decode("#FFFFFF"));
+		accept.setBackground(Color.decode("#071A2B"));
+		panel.add(accept);
+		
+		accept.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				frame.dispose();
+			}
+			
+		});
+		
+	}
+	
+	public void incorrectSignIn() {
+		frame = new JFrame();
+		frame.setSize(700, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setBackground(Color.decode("#FFFCF7"));//FBF3E6
+		panel.setLayout(null);
+		
+		JLabel title = new JLabel("Datos ingresados incorrectos");
+		title.setBounds(50,100,600,70);
+		title.setFont(new Font("Inter_18pt Bold", Font.PLAIN, 32));
+		title.setVisible(true);
+		panel.add(title);
+		
+		JButton accept = new JButton("Aceptar");
+		accept.setBounds(350,350,300,70);
+		accept.setFont(new Font("Inter_18pt Bold", Font.PLAIN, 32));
+		accept.setForeground(Color.decode("#FFFFFF"));
+		accept.setBackground(Color.decode("#071A2B"));
+		panel.add(accept);
+		
+		accept.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				frame.dispose();
+			}
+			
+		});
+	}
+	public void successSignIn() {
+		frame = new JFrame();
+		frame.setSize(700, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setBackground(Color.decode("#FFFCF7"));//FBF3E6
+		panel.setLayout(null);
+		
+		JLabel title = new JLabel("Datos ingresados incorrectos");
+		title.setBounds(50,100,600,70);
+		title.setFont(new Font("Inter_18pt Bold", Font.PLAIN, 32));
+		title.setVisible(true);
+		panel.add(title);
+		
+		JButton accept = new JButton("Aceptar");
+		accept.setBounds(350,350,300,70);
+		accept.setFont(new Font("Inter_18pt Bold", Font.PLAIN, 32));
+		accept.setForeground(Color.decode("#FFFFFF"));
+		accept.setBackground(Color.decode("#071A2B"));
+		panel.add(accept);
+		
+		accept.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				frame.dispose();
+			}
+			
+		});
+	}
 }
